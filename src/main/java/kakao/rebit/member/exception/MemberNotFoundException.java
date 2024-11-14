5be672be2f0fae858c5662e297c17360d89b5fcf
설빈 <138611481+seolbin96@users.noreply.github.com>
2024-11-14 14:05:34 +0900

    [TEST] 카카오 테스트 코드 작성 (#171)
    * feat : RuntimeException 대신 비즈니스 예외 적용
    
    * feat : MemberNotFoundException 추가
    
    * refactor : 비즈니스 예외 사용하도록 수정
    
    * feat : 독서일기에 날짜 입력 속성 추가
    
    * refactor : record 필드에 개행 추가
    
    * Revert "feat : 독서일기에 날짜 입력 속성 추가"
    
    This reverts commit bb9cb31f0c8a99792ea7d0b5f93a16c9b0fc9344.
    
    * test : 카카오 로그인 관련 테스트 코드 작성
    
    * refactor : jwt 의존성 implementation 로 변경

--- src/main/java/kakao/rebit/member/exception/MemberNotFoundException.java
+++ src/main/java/kakao/rebit/member/exception/MemberNotFoundException.java
@@ -3,7 +3,6 @@ package kakao.rebit.member.exception;
 import kakao.rebit.common.exception.BusinessException;
 
 public class MemberNotFoundException extends BusinessException {
-
     public static final BusinessException EXCEPTION = new MemberNotFoundException();
 
     private MemberNotFoundException() {
